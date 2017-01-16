4070470f145f3bd87955839e6a4bffe773e267ed
Rick Powell <richard.powell@sfwltd.co.uk>
2017-01-16 10:08:28 +0000

    Remove old angular version of app

--- old/src/app/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-import 'jquery';
-import 'semantic';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);

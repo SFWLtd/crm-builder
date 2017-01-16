60363a395a3a1b36717be4294d0f41b82de7594d
Rick Powell <richard.powell@sfwltd.co.uk>
2017-01-16 10:08:53 +0000

    Correct loading of App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { App } from "./components/App";
 
 ReactDOM.render(
-    <div>
-        <div class="top-banner">
-            <div class="top-banner-content-container">
-                <img src="sfw-civica.png" id="sfw-logo" class="top-banner-content"/>
-                <p id="title" class="top-banner-content">{{title}}</p>
-            </div>
-        </div>
-
-        <div class="main">
-            <div class="ui vertical menu">
-                <div class="ui dropdown item">
-                    Builds
-                    <i class="dropdown icon"></i>
-                    <div class="menu">
-                        <a class="item">My first build</a>
-                        <a class="item">My second build</a>
-                        <a class="item"><i>Add new...</i><</a>
-                    </div>
-                </div>
-                <a class="item">
-                    Settings
-                </a>
-            </div>
-
-            <div class="divider-container">
-                <div class="ui vertical divider"></div>
-            </div>
-        </div>
-    </div>,
+    <App/>,
     document.getElementById("root")
 );

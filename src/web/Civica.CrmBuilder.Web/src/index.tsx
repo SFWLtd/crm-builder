f6f48107f0e0bb1518ed20de32f33fb74f192fee
Rick Powell <richard.powell@sfwltd.co.uk>
2017-02-01 17:33:27 +0000

    Include material-ui ready for re-styling

--- src/web/Civica.CrmBuilder.Web/src/index.tsx
+++ src/web/Civica.CrmBuilder.Web/src/index.tsx
@@ -3,8 +3,12 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 import appReducer from './reducers/AppReducer';
-
 import App from './containers/App';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 ReactDOM.render(
     <Provider store={createStore(appReducer)}>
--- src/web/Civica.CrmBuilder.Web/src/presentation/App.tsx
+++ src/web/Civica.CrmBuilder.Web/src/presentation/App.tsx
@@ -1,16 +1,19 @@
 ﻿import * as React from 'react';
 import AuthenticationForm from '../containers/authentication/AuthenticationForm';
 import Navigation from '../containers/navigation/Navigation';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export class App extends React.Component<IAppProps, undefined> {
     render() {
-        return <div>
+        return <MuiThemeProvider>
+            <div>
             <Navigation />
             {
                 !this.props.isLoggedIn &&
                 <AuthenticationForm />
-            }                
-        </div>;
+            }
+            </div>
+        </MuiThemeProvider>;
     }
 }
 

1c62cb165a2e619e580079837fb2afe9c75356e7
Rick Powell <richard.powell@sfwltd.co.uk>
2017-02-13 08:43:03 +0000

    Add tests to Protector

---
+++ src/api/Civica.CrmBuilder.Domain.Tests/Protection/ProtectorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Civica.CrmBuilder.Domain.Protection;
+using Xunit;
+
+namespace Civica.CrmBuilder.Domain.Tests.Protection
+{
+    public class ProtectorTests
+    {
+        [Fact]
+        public void ProtectedString_DoesNotEqualUnprotectedString()
+        {
+            var unprotectedString = "Testing 123";
+            var protectedString = Protector.ProtectString(unprotectedString);
+
+            Assert.NotEqual(protectedString, unprotectedString);
+        }
+
+        [Fact]
+        public void ProtectedString_Unprotected_EqualsOriginalUnprotectedString()
+        {
+            var unprotectedString = "Testing 123";
+            var protectedString = Protector.ProtectString(unprotectedString);
+
+            var recoveredString = Protector.UnprotectString(protectedString);
+
+            Assert.Equal(recoveredString, unprotectedString);
+        }
+    }
+}
--- src/api/Civica.CrmBuilder.Domain/Protection/Protector.cs
+++ src/api/Civica.CrmBuilder.Domain/Protection/Protector.cs
@@ -3,22 +3,32 @@ using System.Text;
 
 namespace Civica.CrmBuilder.Domain.Protection
 {
-    public class Protector
+    public static class Protector
     {
         private static byte[] entropy = { 1, 8, 3, 5, 11, 8, 7, 6, 2, 4, 4, 3 };
 
         public static string ProtectString(string plainText)
         {
-            var bytes = Encoding.UTF8.GetBytes(plainText);
+            if (string.IsNullOrEmpty(plainText))
+            {
+                return plainText;
+            }
+
+            var bytes = Encoding.Default.GetBytes(plainText);
             var result = ProtectedData.Protect(bytes, entropy, DataProtectionScope.CurrentUser);
-            return Encoding.UTF8.GetString(result);
+            return Encoding.Default.GetString(result);
         }
 
         public static string UnprotectString(string encryptedString)
         {
-            var bytes = Encoding.UTF8.GetBytes(encryptedString);
+            if (string.IsNullOrEmpty(encryptedString))
+            {
+                return encryptedString;
+            }
+
+            var bytes = Encoding.Default.GetBytes(encryptedString);
             var result = ProtectedData.Unprotect(bytes, entropy, DataProtectionScope.CurrentUser);
-            return Encoding.UTF8.GetString(result);
+            return Encoding.Default.GetString(result);
         }
     }
 }

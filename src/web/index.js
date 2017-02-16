49f6d05d863397cd0191f01cfcf151645b919e0a
Rick <rickpowell1311@gmail.com>
2017-02-16 12:25:11 +0000

    Remove unused files from web project

--- src/web/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var React = require("react");
-var ReactDOM = require("react-dom");
-var App_1 = require("./components/App");
-ReactDOM.render(<App_1.App />, document.getElementById("root"));
-//# sourceMappingURL=index.js.map

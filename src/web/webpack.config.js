f81617234c7eb7f48ca018fdfbe35c1de5487de2
Rick <rickpowell1311@gmail.com>
2017-02-16 10:32:42 +0000

    Switch to using webpack dev server on npm start

--- src/web/webpack.config.js
+++ src/web/webpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: __dirname + "/dist",
+        publicPath: "http://localhost:8080/"
     },
 
     // Enable sourcemaps for debugging webpack's output.
@@ -10,18 +11,13 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
 
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ],
-
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
         ]
     },
 
@@ -31,6 +27,6 @@ module.exports = {
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
         "react": "React",
-        "react-dom": "ReactDOM",
+        "react-dom": "ReactDOM"
     },
 };

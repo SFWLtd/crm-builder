76114c5b10d684e9c0a07e550d0756bedc800135
Rick <rickpowell1311@gmail.com>
2017-02-19 22:54:42 +0000

    Host using Owin web host console app

---
+++ src/api/Civica.CrmBuilder.WebHost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Civica.CrmBuilder.Api;
+using Microsoft.Owin.Hosting;
+
+namespace Civica.CrmBuilder.WebHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "http://localhost:8001";
+            using (WebApp.Start<Startup>(baseUrl))
+            {
+                Console.WriteLine("Press any key to stop");
+                Console.ReadKey();
+            }
+        }
+    }
+}
